df3ba4063e39ddfed37012f52f79164247d729ac
Ryan Thompson <service@aiwebsystems.com>
2015-07-17 12:54:45 -0500

    Initial

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'name'        => 'Landed',
+    'description' => 'A dark, slick, modern, responsive, adjective-drenched theme by HTML5 UP!'
+];
---
+++ src/LandedTheme.php
@@ -0,0 +1,16 @@
+<?php namespace Anomaly\LandedTheme;
+
+use Anomaly\Streams\Platform\Addon\Theme\Theme;
+
+/**
+ * Class LandedTheme
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\LandedTheme
+ */
+class LandedTheme extends Theme
+{
+
+}
